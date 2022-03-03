5ad15fae6db42edec1745d5c545491bee4ad2b8b
Wafi Hussain <66385207+WafiHussain@users.noreply.github.com>
2022-03-02 20:21:36 -0600

    Create GuessthenumberApplication.java

---
+++ GuessTheNumber/src/main/java/sg/guessthenumber/GuessthenumberApplication.java
@@ -0,0 +1,13 @@
+package com.sg.guessthenumber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuessthenumberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuessthenumberApplication.class, args);
+	}
+
+}
