320660d2a00ad2471fd5c6488f71ef55d2dbada3
Wafi Hussain <66385207+WafiHussain@users.noreply.github.com>
2022-03-03 09:25:25 -0600

    Add files via upload

---
+++ jQuery-HTML-Elements/js/home.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+    // Handler
+    $('#centerUp').on('click', function() {
+        $('h1').addClass('text-center');
+        $('h2').addClass('text-center');
+    });
+
+    // Replace classes
+    
+});
